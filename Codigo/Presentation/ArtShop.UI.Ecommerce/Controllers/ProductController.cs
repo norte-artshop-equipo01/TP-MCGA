3f87a73a3d5a6d150730ce3abafd513c996cc630
Emmanuel Leardi <emmaleardi@hotmail.com>
2020-10-26 01:49:23 -0300

    product detail agregado, link desde la imagen del index funciona, falta corregir carousel.

---
+++ Codigo/Presentation/ArtShop.UI.Ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using ArtShop.Entities.Model;
+using ArtShop.UI.Process;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtShop.UI.Ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductProcess prd = new ProductProcess();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public void ProductDetail2(int id)
+        {
+            var prod = prd.GetById(id);
+            ProductDetail(prod);
+        }
+        public ActionResult ProductDetail(Product product)
+        {
+            
+            
+            return View(prd.GetById(product.Id));
+        }
+    }
+}
