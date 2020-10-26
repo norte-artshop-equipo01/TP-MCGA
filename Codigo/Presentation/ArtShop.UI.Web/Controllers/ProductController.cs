3f844338d64e4178e5795860d043e9ee13eebec0
Emmanuel Leardi <emmaleardi@hotmail.com>
2020-10-26 20:30:21 -0300

    cambios en el layout y agergado de ProductController e index del mismo

---
+++ Codigo/Presentation/ArtShop.UI.Web/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using ArtShop.UI.Process;
+using ArtShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtShop.UI.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductProcess prd = new ProductProcess();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(prd.ListarTodosLosProductos());
+        }
+
+        
+    }
+}
